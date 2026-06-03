97c23058cf90fbf1ae7e3c1912ce809cce5f4ec5
Wesley <w.poth1001@gmail.com>
2026-06-03 11:14:15 +0200

    changed scopes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,23 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
+const scopes = [
+  "user-read-email",
+  "user-read-private",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+];
+
 const handler = NextAuth({
+  
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID!,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
       authorization:
-        "https://accounts.spotify.com/authorize?scope=user-read-email user-read-private playlist-read-private playlist-read-collaborative user-read-playback-state user-modify-playback-state",
+        "https://accounts.spotify.com/authorize?scope=" + scopes.join(" "),
     }),
   ],
 
